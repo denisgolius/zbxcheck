75dd860d76a1dd6b2b203243ff8331eb59a23f98
zyongqing <zyongqing@gmail.com>
2014-10-16 21:51:36 +0800

    split vmware vsphere hardware check into two files

--- lib/snmplib.py
+++ lib/snmplib.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
-
+#
+# 1.0.1 Notes:
+# reraise all exceptions to caller
+#
 import time
 import json
 import netsnmp
@@ -8,7 +11,7 @@ import netsnmp
 # Ref doc:
 # https://net-snmp.svn.sourceforge.net/svnroot/net-snmp/trunk/net-snmp/python/README
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 verbose = False
 
 
@@ -22,34 +25,30 @@ def snmp_query(desthost, community, oid, condition=None):
     vlist = netsnmp.VarList()
     vlist.append(vbind)
 
-    try:
-        netsnmp.snmpwalk(
-            vlist,
-            Version=2,
-            DestHost=desthost,
-            Community=community,
-            UseNumeric=1)
-        log('oid %s have %d items before filter.' % (oid, len(vlist)))
+    netsnmp.snmpwalk(
+        vlist,
+        Version=2,
+        DestHost=desthost,
+        Community=community,
+        UseNumeric=1)
+    log('oid %s have %d items before filter.' % (oid, len(vlist)))
 
-        # result format:
-        # {
-        #    "INDEX": "VALUE",
-        #    "INDEX": "VALUE",
-        # }
-        result = {}
-        for v in vlist:
-            if condition is None or condition(v.val):
-                full_oid = "%s.%s" % (v.tag, v.iid)
-                # INDEX is:
-                # "tag.iid" - "oid."
-                index = full_oid[len(oid)+1:]
-                result[index] = v.val
+    # result format:
+    # {
+    #    'INDEX': 'VALUE',
+    #    'INDEX': 'VALUE',
+    # }
+    result = {}
+    for v in vlist:
+        if condition is None or condition(v.val):
+            full_oid = '%s.%s' % (v.tag, v.iid)
+            # INDEX is:
+            # 'tag.iid' - 'oid.'
+            index = full_oid[len(oid)+1:]
+            result[index] = v.val
 
-        log('oid %s have %d items after filter.' % (oid, len(result)))
-        return result
-    except Exception as e:
-        log('oid %s occure unexpected error: %s' % (oid, e))
-        return {}
+    log('oid %s have %d items after filter.' % (oid, len(result)))
+    return result
 
 
 def lld_format(items):
@@ -68,14 +67,14 @@ def lld_process(hostname, community, rule):
     #
     # example:
     #    (
-    #        ".1.3.6.1.2.1.2.2.1.2",
-    #        [(".1.3.6.1.2.1.2.2.1.8", lambda x: x == '1')]
+    #        '.1.3.6.1.2.1.2.2.1.2',
+    #        [('.1.3.6.1.2.1.2.2.1.8', lambda x: x == '1')]
     #    )
     #
     # you can also get origin_oid without filters
     # but we recommand you using zabbix snmp lld:
     #    (
-    #        ".1.3.6.1.2.1.2.2.1.2",
+    #        '.1.3.6.1.2.1.2.2.1.2',
     #        []
     #    )
     origin_oid, filter_oids = rule
@@ -96,8 +95,8 @@ def lld_process(hostname, community, rule):
                 break
         else:
             item = {}
-            item["{#SNMPINDEX}"] = index
-            item["{#SNMPVALUE}"] = origin_oid_items[index]
+            item['{#SNMPINDEX}'] = index
+            item['{#SNMPVALUE}'] = origin_oid_items[index]
             result.append(item)
 
     return result
